e1a3d386841fdd62b970291739de3a38ae4bc896
Jaime Sarrion Sahuquillo <jss72@alu.ua.es>
2019-09-04 20:51:12 +0200

    Subida de control por cambio de ordenador

--- Argos/src/components/DetallePaciente.js
+++ Argos/src/components/DetallePaciente.js
@@ -3,7 +3,9 @@ import React from 'react';
 import {
     StyleSheet,
     Text,
-    View
+    View,
+    Image,
+    TouchableHighlight,
 } from 'react-native';
 
 
@@ -19,16 +21,55 @@ class DetallePaciente extends React.Component {
             color: 'black',
         },
     };
-    componentWillMount(){
-        this.setState({paciente: this.props.navigation.state.params.paciente})
+    componentWillMount() {
+        this.setState({ paciente: this.props.navigation.state.params.paciente })
     }
-    render(){
-                return(
-            <View>
-            <Text>
-             {this.state.paciente.nombre}
-            </Text>
-            </View >
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Image
+                        source={require('../images/user.png')}
+                        style={styles.imagen}
+                    ></Image>
+                    <View >
+                        <Text style={styles.nombre}>
+                            {this.state.paciente.nombre}
+                        </Text>
+                        <Text>
+                            {this.state.paciente.apellidos}
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.body}>
+                    <View style={flexDirection = "row"}>
+                        <TouchableHighlight>
+                            <Text>BPM</Text>
+                        </TouchableHighlight>
+                        <TouchableHighlight>
+                            <Text>Pasos</Text>
+                        </TouchableHighlight>
+                    </View>
+                    <View style={flexDirection = "row"}>
+                        <TouchableHighlight>
+                            <Text>Temperatura</Text>
+                        </TouchableHighlight>
+                        <TouchableHighlight>
+                            <Text>Oxigeno</Text>
+                        </TouchableHighlight>
+                    </View>
+                </View>
+                <View style={styles.footer}>
+                    <Text style={styles.nombre}>
+                        Observaciones:
+                    </Text>
+                    <Text>
+                        {this.state.paciente.observaciones}
+                    </Text>
+                </View>
+            </View>
+
+
         )
     }
 }
@@ -37,8 +78,29 @@ class DetallePaciente extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#2893d3',
+        paddingLeft: 35,
+        paddingRight: 35,
     },
+    nombre: {
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    imagen: {
+        width: 100,
+        height: 100
+    },
+    header: {
+        flexDirection: "row"
+    },
+    body: {
+        backgroundColor: 'white'
+    },
+    boton: {
+
+    },
+    footer: {
+        backgroundColor: "grey"
+    }
 })
 module.exports = DetallePaciente
